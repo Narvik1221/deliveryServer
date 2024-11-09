ec31aebf1cb0d0536044534c9f091e8ad7ed4c38
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2024-11-09 16:51:23 +0600

    321

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,9 @@
-const Router = require('express')
-const router = new Router()
-const userController = require('../controllers/userController')
-const authMiddleware = require('../middleware/authMiddleware')
+const Router = require("express");
+const router = new Router();
+const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post('/registration', userController.registration)
-router.post('/login', userController.login)
-router.get('/auth', authMiddleware, userController.check)
+router.post("/login", userController.login);
+router.get("/auth", authMiddleware, userController.check);
 
-
-
-
-module.exports = router
+module.exports = router;
